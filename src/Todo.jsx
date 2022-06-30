f9c0cbf968415b5302022a7017706e7de4ddd31d
thabata <thabatagcampos@gmail.com>
2022-06-30 19:11:19 -0300

    update

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,63 +1,85 @@
 import { useState } from "react";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { GetAllTodos } from "./mocks/Todomock"
 
 function Todo(){
-const [state, setState] = useState(GetAllTodos);
-const [list, setList] = useState([]); 
+const [task, setTask] = useState([...GetAllTodos]);
+const [newTask, setNewTask] = useState(""); 
 
-// useEffect é um hook que lida com os efeitos colaterais 
-// ele lida com os ciclos de vida do componente
-// 2 argumentos: 1. função callback (efeito) 2. array de dependências
-useEffect(()=>{
-    //1. capturar o valor atual do meu contador
-    console.log("Valor atual do contador:", state)
-    // 2. atualizar minha lista com os valores do contador (adicionar)
-    const newArr = [...list]
-    newArr.push(state)
-    // 3. fazer o state "list" ser igual à essa nova lista
-    setList(newArr)
-}, [state]) 
+// const handleShowTodo = (event) => {
+//   const clone = [...GetAllTodos]
+//   const aux = clone.splice(5,5)
+//   console.log(clone)
+//   console.log(aux)
+
+//   // setListas(clone.splice(5,5))
+//   setTask(clone)
+// }
+
+const addNewTodo = (event) => {
+  let new_todo = {
+    // id: "",
+    text: newTask,
+    // completed: false,
+  }
+
+  const clone2 = [...task]
+  clone2.push(new_todo)
+  setTask(clone2)
+  setNewTask("")
+}
+
+const handleSubmit = (event) => {
+  console.log(event.target.value)
+  event.preventDefault()
+  setNewTask(event.target.value)
+}
+
+// // useEffect é um hook que lida com os efeitos colaterais 
+// // ele lida com os ciclos de vida do componente
+// // 2 argumentos: 1. função callback (efeito) 2. array de dependências
+
+// useEffect(()=>{
+// //     //1. capturar o valor atual do meu contador
+//     console.log("Valor atual do contador:", task)
+// //     // 2. atualizar minha lista com os valores do contador (adicionar)
+//     const newArr = [...task]
+//     newArr.push(task)
+// //     // 3. fazer o state "list" ser igual à essa nova lista
+//     setNewTask(newArr)
+// }, [task]); 
 
 
-console.log("state:", state);
+console.log("task:", task);
 console.log("mock: ", GetAllTodos);
 
 
-
-function handleSubmit(event) {
-    event.preventDefault()
-
-// const newTodo = {
-//     id: 
-//     text: state
-//     completed: false
+// function handleSubmit(event) {
+//     event.preventDefault()
 // }
 
-}
-
 return (
-    <div>
-        <h1>Todo List </h1> 
-        <form onSubmit={handleSubmit} className="form">
-            <label>Add Todo</label>
-            <input type="text" />
-            <button type="submit">Submit</button>
-        </form>
-    </div>
+  <div>
+    <h1>Todo List </h1> 
+    <form className="form">
+        <label>Add Todo</label>
+        <input onChange={handleSubmit} type="text" value={newTask} />
+        <button onClick={addNewTodo} type="submit">Submit</button>
+    </form>
+
+  <section>
+    {task.map((states) => {
+    return (
+      <div>
+        text={states.text}
+      </div>
+    );
+  })}
+</section> 
+</div>
 ); 
+
 };
 
 export default Todo;
 
-<section>
-{state.map((states) => {
-  return (
-    <div>
-      id={states.id}
-      text={states.text}
-      completed={states.completed}
-    </div>
-  );
-})}
-</section>
