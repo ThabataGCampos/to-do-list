9fa0b2fadf8dd0bdf6f6c4ca76cbbf10db39aabc
Thabata Gomes de Campos <thabatagcampos@gmail.com>
2022-07-14 16:36:42 -0300

    att checkbox

---
+++ src/components/Navbar/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return(<footer>
+        <h4>
+            <p>Desenvolvido por: <a href="https://github.com/ThabataGCampos" rel="noreferrer" target="_blank">Thabata G Campos</a> </p>
+        </h4>
+        </footer>
+    )
+}
+    
+export default Footer;
+
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './components/Navbar/Header';
 import Todo from './components/Todo';
+import Footer from './components/Navbar/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
     <Todo />
+    <Footer/>
   </React.StrictMode>
 );
 
