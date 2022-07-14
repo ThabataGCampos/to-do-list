bdaaba0cf37a246b988a62d2775ef9343e9f5e85
Thabata Gomes de Campos <thabatagcampos@gmail.com>
2022-07-13 23:40:51 -0300

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -23,7 +23,7 @@ function Todo() {
 
   const [todoEditing, setTodoEditing] = useState (false); 
 
-  // flag para o modal de deletar uma Paleta
+  // flag para o modal de deletar 
   const [showDeleteModal, setShowDeleteModal] = useState(false)
 
   const baseURL= "http://localhost:8000/alltodo";
@@ -135,7 +135,7 @@ function Todo() {
 
   const handleClickDeleteWarning = (e) => {
     setShowDeleteModal(true)
-    setTask({paleta_id: e.target.id})
+    setTask({task_id: e.target.id})
 }
 
   const handleClickDelete = (e) => {
@@ -246,7 +246,6 @@ const closeModalDelete = () => {
                       ? <FormControl 
                       id={task.id}
                       key={task.id}
-                      // htmlFor={task.id}
                       onChange={handleChangeEdit}
                       type="text"
                       name="text"
@@ -262,9 +261,7 @@ const closeModalDelete = () => {
             name="done"
             checked={task.done}
             />
-          {/* {task.done 
-                      ? <p className="card-text">done</p>
-                      : <p className="card-text">To do</p>}  */}
+  
           <svg id={task.id} onClick={handleClickEdit} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="iconEd" viewBox="0 0 16 16">
           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
           <path  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
