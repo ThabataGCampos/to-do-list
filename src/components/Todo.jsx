3a5c145df25328db67cd34aba20b52d5ee7b7910
Thabata Gomes de Campos <thabatagcampos@gmail.com>
2022-07-15 16:21:02 -0300

    Finalizado (alteração no edit e botão de return)

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -29,9 +29,10 @@ function Todo() {
   // flag para o modal de deletar 
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
+  // flag para o para exibir o formulário do search by id
   const [showSearch, setShowSearch] = useState(false);
 
-  const baseURL= "http://localhost:8000/alltodo";
+  // const baseURL= "http://localhost:8000/alltodo";       //foi substituido pelo TaskService e Api Helper
 
   async function findAllTasks() {
     // const response = await fetch(baseURL);          //foi substituido pelo TaskService e Api Helper
@@ -98,9 +99,12 @@ function Todo() {
     findOneTask(task_id_search);
   };
 
-  const handleClickSearchForm = () => {     // para aparecer o formulário com o imput
-    setShowSearch(true)
+  const handleClickSearchForm = () => {   
+    setShowSearch(true);  // para aparecer o formulário com o imput
   }
+  const handleClickReturn = () => {      
+    findAllTasks(); // para aparecer todas as tasks
+  };
 
   const handleChangeCreate = (event) => {
     setNewTask({...newTask, [event.target.name]: event.target.value});
@@ -140,19 +144,13 @@ function Todo() {
     updateTask(id, task_being_edited);
   };
 
-  async function findEditTask(id){
-    const response = await fetch(`${baseURL}/${id}`);
-    const task = await response.json();
-    setEditedTask({...editedTask, ...task});
-  };
-
-  // botão icone editar
-  const handleClickEdit = (event) => {
-    setEditedTask({...editedTask, id: event.target.id});
-    findEditTask(event.target.id);
+  const handleClickEdit = async (event) => {
     setTodoEditing(true, {id: event.target.id});
+    setEditedTask ({ ...editedTask, id: event.target.id});
+    const task = await TaskService.getById(event.target.id);
+    setEditedTask ({ ...editedTask, ...task});
   };
-
+ 
   const handleChangeCheckBox = async (event) => {
     const checkbox_value = event.target.checked;
     const task_to_be_updated = await TaskService.getById(event.target.id);
@@ -165,18 +163,16 @@ function Todo() {
     findAllTasks();
   }, [newTask, editedTask]);
 
-  console.log(taskList);
-
   return (
     <div className="allform">
       <FormControl 
-        id="criar_descrição"
-        label="Add a new task"
-        className="FormSeachAndCreate"
-        onChange={handleChangeCreate}
-        type="text"
-        name="text"
-        value={newTask.text} 
+      id="criar_descrição"
+      label="Add a new task"
+      className="FormSeachAndCreate"
+      onChange={handleChangeCreate}
+      type="text"
+      name="text"
+      value={newTask.text} 
       />
       <button className="btn" onClick={handleCreateTask}>Add</button>  
       {/* exemplo inativo
@@ -197,7 +193,8 @@ function Todo() {
           name="task_id"
           value={task.task_id}
           />
-        <button type="button" className={`btn`} onClick={handleClickSearch}>Search</button> </div>
+        <button type="button" className={`btn`} onClick={handleClickSearch}>Search</button>
+        <button type="button" className={`btn`} onClick={handleClickReturn}>Return</button> </div>
         : <button type="button" className={`btnSearch`} onClick={handleClickSearchForm}>Search by ID</button>
       }
 
@@ -226,24 +223,38 @@ function Todo() {
               value={editedTask.text} 
               />
             : <CheckBoxControl
-            id={task.id}
-            label={task.text}
-            onChange={handleChangeCheckBox}
-            name="done"
-            checked={task.done}
-            />} 
+              id={task.id}
+              label={task.text}
+              onChange={handleChangeCheckBox}
+              name="done"
+              checked={task.done}
+              />} 
 
           {todoEditing === true && editedTask.id === task.id
             ? (<button className="btn_editText" onClick={handleUpdateTask}>Edit</button>)   
           
             : (<div className="todo_edits">
               {/* icone edit */}
-              <svg id={task.id} onClick={handleClickEdit} xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="iconEd" viewBox="0 0 16 16">
+              <svg id={task.id}
+              onClick={handleClickEdit} 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="22" 
+              height="22" 
+              fill="currentColor" 
+              className="iconEd" 
+              viewBox="0 0 16 16">
               <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
               <path  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg> 
               {/* icone delet */}
-              <svg  id={task.id} onClick={handleClickDeleteWarning} xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="iconTr" viewBox="0 0 16 16">
+              <svg  id={task.id} 
+              onClick={handleClickDeleteWarning} 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="22" 
+              height="22" 
+              fill="currentColor" 
+              className="iconTr" 
+              viewBox="0 0 16 16">
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
               </svg> 
           </div>)}  
